490582256349b1d41730743dd44ad12089429c9d
akanseitzhanov <akanseitzhanov@yandex.ru>
2020-05-13 04:35:06 +0600

    removed seed migration command

--- routes/web.php
+++ routes/web.php
@@ -25,10 +25,6 @@ Route::post('subscriber', 'SubscriberController@store')->name('subscriber.store'
 Route::get('/search', 'SearchController@search')->name('search');
 
 Route::get('/migrate', function () {
-    Artisan::call('migrate --seed');
-});
-
-Route::get('/migrate', function () {
     Artisan::call('migrate');
 });
 
